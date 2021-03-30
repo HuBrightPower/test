8346a662c536b703fa36a865c36c0f769b3f4fef
hu <1040358185@qq.com>
2021-03-30 20:41:27 +0800

    项目hello

---
+++ hello/src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author humingwei
+ * @class Hello
+ * @time 2021/3/30
+ */
+public class Hello {
+     public static void main(String[] args) {
+         System.out.println("hello");
+         }
+}
