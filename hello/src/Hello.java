21c667c3795b55d985e93b6640c882275dbc45b2
hu <1040358185@qq.com>
2021-03-31 17:07:49 +0800

    老人删库跑路，只剩一个文件

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -5,9 +5,7 @@
  */
 public class Hello {
      public static void main(String[] args) {
-         System.out.println("hello");
-         System.out.println("第一次修改");
-         System.out.println("新人第一次修改");
-System.out.println("老人对第一个文件修改");
+         System.out.println("我删库跑路了");
+
          }
 }
